fedca8cbc0916a448b766b4fff8dc5a437d1555a
Neha Rani <45110927+neha1010@users.noreply.github.com>
2019-10-24 16:37:14 -0400

    Update SequencePartioning.java

--- SequencePartioning.java
+++ SequencePartioning.java
@@ -26,4 +26,39 @@ public class Main
 		}
 		
 	}
+	// Load sequences from input file
+	public boolean loadSequencesFromFile(){
+		// TO DO: Add more validations for read lines?
+		allSequences=new ArrayList<>();
+		try(
+			BufferedReader br = new BufferedReader(new FileReader(infilename))) {
+			String line = br.readLine();
+			while (line != null) {
+				if(line.matches("[ACGT]+")) allSequences.add(line);
+				line = br.readLine();
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+			System.out.println("IO Exception! Error reading sequences from input file!");
+			return false;
+		}
+		return true;
+	}
+	// write final sequence to file
+	public void writeToOutfile(){
+		try{
+			//System.out.println(outfilename);
+			PrintWriter writer = new PrintWriter(outfilename);
+			writer.println(finalSequence);
+			writer.close();
+			
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+			System.out.println("IO Exception! Error writing output to file!");
+		}
+	}
+	
+	
 }
